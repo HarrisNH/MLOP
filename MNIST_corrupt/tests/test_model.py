954c12fc8bcfac8f335c9f1f96fdedef7f4b5669
HarrisNH <s224397@dtu.dk>
2025-01-14 09:13:49 +0100

    update requirements file

--- MNIST_corrupt/tests/test_model.py
+++ MNIST_corrupt/tests/test_model.py
@@ -8,12 +8,7 @@ def test_model():
     y = model(x)
     assert y.shape == (1, 10)
 
-def test_error_on_wrong_shape():
-    model = MyAwesomeModel()
-    with pytest.raises(ValueError, match='Expected input to a 4D tensor'):
-        model(torch.randn(1,1,28,28))
-    with pytest.raises(ValueError, match='Expected each sample to have shape [1, 28, 28]'):
-        model(torch.randn(1,1,28,28))
+
 
 @pytest.mark.parametrize("batch_size", [32, 64])
 def test_model(batch_size: int) -> None:
